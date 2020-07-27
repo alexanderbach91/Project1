a2ce93d0cc0bbe0d700c168bc9ae85a9cc0a8115
Alex <alexbachwork@gmail.com>
2020-07-27 14:32:21 +0300

    improvedwithContinue

--- script.js
+++ script.js
@@ -14,19 +14,35 @@ const personalMovieDB = {
 
 };
 
-for (let i = 1; i <= 2; i++) {
+// for (let i = 1; i <= 2; i++) {
+//     const answer = prompt("Один из последних просмотренных фильмов?","");
+//     const answer2 = prompt("На сколько оцените его?", "");
+
+//     if (answer !== "" && answer2 !== "" && answer !== null && answer2 !== null && answer.length < 50) {
+//         personalMovieDB.movies[answer] = answer2;
+//         console.log("good");
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+
+// }
+
+for (let i = 0; i < 2; i++){
     const answer = prompt("Один из последних просмотренных фильмов?","");
     const answer2 = prompt("На сколько оцените его?", "");
 
-    if (answer !== "" && answer2 !== "" && answer !== null && answer2 !== null && answer.length < 50) {
-        personalMovieDB.movies[answer] = answer2;
-        console.log("good");
-    } else {
-        console.log("error");
+    if (answer === "" || answer2 === "" || answer === null || answer2 === null || answer.length  > 50) {
         i--;
+        console.log("error");
+        continue;
+    } else {
+        console.log("good");
+        personalMovieDB.movies[answer] = answer2;
     }
-
 }
+
+
  if (personalMovieDB.count < 10) {
      alert("Просмотрено довольно мало фильмов");
  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
